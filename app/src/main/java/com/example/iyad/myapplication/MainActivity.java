cc12256b3977bd0f72809760a08aae2c69459285
iyadjamal <iyadjamal5@gmail.com>
2018-02-02 21:48:21 +0300

    Load text from file then and show it on edit text

--- app/src/main/java/com/example/iyad/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iyad/myapplication/MainActivity.java
@@ -8,8 +8,12 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import java.io.BufferedReader;
+import java.io.DataInputStream;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.InputStreamReader;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -17,6 +21,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         addButtonListener();
+        loadSavedFile();
+    }
+    private void loadSavedFile()
+    {
+        EditText Notetxt = (EditText)findViewById(R.id.note);
+        try {
+            FileInputStream Fis = openFileInput("1stNote");
+            BufferedReader Dis = new BufferedReader(new InputStreamReader(new DataInputStream(Fis)));
+            String LoadedText,s="";
+            while ((LoadedText = Dis.readLine())!=null)
+            {
+                s+=LoadedText;
+            }
+            Dis.close();
+            Fis.close();
+            Notetxt.setText(s);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     private void addButtonListener()
     {
