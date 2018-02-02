87912df9ef22979c215363e2e7f0bed5f5c2a88a
iyadjamal <iyadjamal5@gmail.com>
2018-02-02 21:07:13 +0300

    Add listener and save text from edit text to file

--- app/src/main/java/com/example/iyad/myapplication/MainActivity.java
+++ app/src/main/java/com/example/iyad/myapplication/MainActivity.java
@@ -1,13 +1,39 @@
 package com.example.iyad.myapplication;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        addButtonListener();
+    }
+    private void addButtonListener()
+    {
+        Button Savebtn=(Button) findViewById(R.id.save);
+        Savebtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v)
+            {
+                EditText Notetxt = (EditText)findViewById(R.id.note);
+                String SavedNote=Notetxt.getText().toString();
+                try {
+                    FileOutputStream Fos=openFileOutput("1stNote", Context.MODE_PRIVATE);
+                    Fos.write(SavedNote.getBytes());
+                    Fos.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
